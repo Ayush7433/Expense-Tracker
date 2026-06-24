7eb85de7cdcbc46373772a1f63b999f5f3a36058
Ayush7433 <maheraayush2302@gmail.com>
2026-06-25 02:13:18 +0530

    creted get, update and delete expense controller and routes

--- server/controllers/expenseController.js
+++ server/controllers/expenseController.js
@@ -1,38 +1,123 @@
-const Expense = require('../models/Expense');
+const Expense = require("../models/Expense");
 
 const createExpense = async (req, res) => {
+  try {
+    const { title, amount, category, description, expenseDate } = req.body;
+    if (!title || !amount || !category) {
+      return res.status(400).json({
+        success: false,
+        message: "Title, amount and category are required",
+      });
+    }
+
+    const user = req.user;
+
+    const expense = await Expense.create({
+      title,
+      amount,
+      category,
+      description,
+      expenseDate,
+      user: user._id,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "Expense created successfully",
+      expense,
+    });
+  } catch (error) {
+    res.staus(501).json({
+      success: false,
+      message: "Error creating expense",
+    });
+  }
+};
+
+const getExpenses = async (req, res) => {
+  try {
+    const user = req.user;
+
+    const expenses = await Expense.find({ user: user._id }).sort({
+      createdAt: -1,
+    });
+    res.status(200).json({
+      success: true,
+      expenses,
+      count: expenses.length,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error fetching expenses",
+    });
+  }
+};
+
+const updateExpense = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const expense = await Expense.findOne({
+      _id: id,
+      user: req.user._id,
+    });
+
+    if (!expense) {
+      return res.status(404).json({
+        success: false,
+        message: "Expense not found",
+      });
+    }
+
+    const updatedExpense = await Expense.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+        success: true,
+        message: "Expense updated successfully",
+        expense: updatedExpense
+    })
+  } catch (error) {
+    res.status(500).json({
+        success: false,
+        message: "Error updating expense" + error,
+    })
+  }
+};
+
+const deleteExpense = async (req, res) => {
     try {
-        const {title, amount, category, description, expenseDate} = req.body;
-        if(!title || !amount || !category ){
-            return res.status(400).json({
-                success: false,
-                message: "Title, amount and category are required"
-            })
+        const {id} = req.params;
+        const expense = await Expense.findOne({
+            _id : id,
+            user : req.user._id
+        });
+
+        if(!expense){
+            return res.status(404).json({
+                success : false,
+                message : "Expense not found"
+            });
         }
 
-        const user = req.user;
+        const deletedExpense = await expense.deleteOne();
 
-        const expense = await Expense.create({
-            title,
-            amount,
-            category,
-            description,
-            expenseDate,
-            user : user._id,
-        })
+        res.status(200).json({
+            success : true,
+            message : "Expense deleted successfully",
+        });
 
-        res.status(201).json({
-            success: true,
-            message: "Expense created successfully",
-            expense
-        })
-
+        
     } catch (error) {
-        res.staus(501).json({
-            success: false,
-            message: "Error creating expense",
+        res.status(500).json({
+            success : false,
+            message : "Error deleting expense" + error
         })
+        
     }
 }
 
-module.exports = {createExpense}
+module.exports = { createExpense, getExpenses, updateExpense, deleteExpense };
--- server/routes/expenseRoutes.js
+++ server/routes/expenseRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const { createExpense } = require('../controllers/expenseController');
+const { createExpense, getExpenses, updateExpense, deleteExpense } = require('../controllers/expenseController');
 const authMiddleware = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.post('/expenses', authMiddleware, createExpense);
+router.get('/expenses', authMiddleware, getExpenses);
+router.put('/expenses/:id', authMiddleware, updateExpense);
+router.delete('/expenses/:id', authMiddleware, deleteExpense)
 
 
 module.exports = router;
