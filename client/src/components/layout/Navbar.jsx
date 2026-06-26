2a7aec16bd57953cb91c64ed79b2bc8f80536fcd
Ayush7433 <maheraayush2302@gmail.com>
2026-06-26 17:13:45 +0530

    created navbar structure

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import { Bell, Menu } from "lucide-react";
+import React from "react";
 
 const Navbar = () => {
   return (
-    <header className='h-16 bg-white border-b px-4 flex items-center'>
-      <h2 className='text-lg font-semibold'>Navbar</h2>
+    <header className="h-16 bg-white border-b px-4 flex items-center justify-between">
+      <div className="flex gap-3 items-center">
+        <div>
+          <button className="cursor-pointer hover:text-gray-600">
+            <Menu size={16} />
+          </button>
+        </div>
+      </div>
+      <div className="flex gap-3 items-center">
+        <div>
+          <input
+            type="search"
+            name="search"
+            id="search"
+            placeholder="Search expenses..."
+            className="border focus:outline-none rounded-sm px-2 text-sm py-1 focus:ring-2 focus:ring-blue-400"
+          />
+        </div>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
